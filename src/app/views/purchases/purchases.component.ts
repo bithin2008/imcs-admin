2ab2ce76ae10e5a5c5f150ccb1fcebc27bc444ed
BITHIN <bithin.softronet@gmail.com>
2022-06-12 12:13:32 +0530

    update

--- src/app/views/purchases/purchases.component.ts
+++ src/app/views/purchases/purchases.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { WebService } from 'src/app/service/common-service';
 @Component({
   selector: 'app-purchases',
   templateUrl: './purchases.component.html',
@@ -10,42 +11,47 @@ export class PurchasesComponent implements OnInit {
   printModalRef?: BsModalRef;
   viewModalRef?: BsModalRef;
   purchaseModalRef?: BsModalRef;
-  public purchaseList: any = [
-    {
-      "vendorName": "Fairmont creations",
-      "vendorNumber": "1003",
-      "purchaseDate": "2022-04-28"
-    },
-    {
-      "vendorName": "Tilak Jewelers",
-      "vendorNumber": "5003",
-      "purchaseDate": "2022-03-15"
-    }, {
-      "vendorName": "HEENA GEMS",
-      "vendorNumber": "1010",
-      "purchaseDate": "2022-03-22"
-    },
-    {
-      "vendorName": "SHRI VARDHAMAN JEWELERS",
-      "vendorNumber": " 10049 ",
-      "purchaseDate": "2022-02-25"
-    },
+  filterForm: any = {
+    searchText: '',
+  };
 
-  ];
 
   public pageOfItems: any = [];
-  public items: any = [];
+  public purchaseList: any = [];
 
-  constructor(private router: Router, private modalService: BsModalService) { }
+  constructor(private router: Router, private modalService: BsModalService, private webService: WebService) { }
 
   ngOnInit(): void {
 
-    this.items = Array(50).fill(0).map((x, i) => ({
-      "vendorName": `Tilak Jewelers ${i + 1}`,
-      "vendorNumber": `500${i + 1}`,
-      "isLocked": false,
-      "purchaseDate": `2022-03-${i + 1}`
-    }));
+    // this.items = Array(50).fill(0).map((x, i) => ({
+    //   "vendorName": `Tilak Jewelers ${i + 1}`,
+    //   "vendorNumber": `500${i + 1}`,
+    //   "isLocked": false,
+    //   "purchaseDate": `2022-03-${i + 1}`
+    // }));
+
+    this.getSubscriptionList();
+  }
+
+  getSubscriptionList() {
+    // this.spinnerService.show();
+    let url = `tilak/purchaseOrders`;
+    if (this.filterForm.searchText)
+      url = url + `&searchText=${this.filterForm.searchText}`;
+    this.webService.get(url).subscribe((response: any) => {
+      //  this.spinnerService.hide();
+      if (response.length > 0) {
+        this.purchaseList = response;
+        // this.purchaseList = response.forEach(element => {
+        //   Array(response.length).fill(0).map((x, i) => (element));
+        // });
+
+      } else {
+        this.purchaseList = [];
+      }
+    }, (error) => {
+      console.log('error', error);
+    });
   }
 
   createInvoice() {
